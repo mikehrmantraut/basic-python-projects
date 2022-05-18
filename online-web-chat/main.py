ef358f24eecf60fbd7cb5127b85dae3bdaf0e8ea
Mehmet Baran Munar <76072666+mikehrmantraut@users.noreply.github.com>
2022-05-18 23:14:17 +0300

    Update main.py

--- online-web-chat/main.py
+++ online-web-chat/main.py
@@ -1,1 +1,22 @@
+from socket import socket
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 
+app = Flask(__name__)
+app.config['SECRET'] = "secret!123"
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@socketio.on('message')
+def handle_message(message):
+    print("Received message: " + message)
+    if message != "User connected!":
+        send(message, broadcast=True)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    # your ipv4 address
+    socketio.run(app, host="192.168.0.165")
